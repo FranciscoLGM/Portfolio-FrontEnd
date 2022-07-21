df6f96d38612acd274d3d703f44b6a05e0e1391a
Francisco Javier López García Mayorga <javier6282@hotmail.com>
2022-07-20 21:49:09 -0300

    [UPDATE] Front End structure of the Integrator practical work is created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExpertiseComponent } from './components/expertise/expertise.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ExpertiseComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/assets/js/menu.js
@@ -0,0 +1,6 @@
+function onClick() {
+    console.log("click");
+    document.getElementById("menu").classList.toggle("active");
+    document.getElementById("nav").classList.toggle("active");
+    document.getElementById("menu-bg").classList.toggle("change");
+}
